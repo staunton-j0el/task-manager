8c91a194ec1f6d1689ebab242d2776a02b34e6b2
staunton-j0el <staunton.joel@gmail.com>
2025-08-22 08:45:33 +1000

    day 3: using flask for simple API

---
+++ task_manager_day3/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+tasks = [
+    {"id": 1, "title": "Buy milk", "done": False},
+    {"id": 2, "title": "Clean desk", "done": False},
+    {"id": 3, "title": "Read book", "done": True},
+]
+
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    return jsonify(tasks)
+
+@app.route("/tasks/<int:id>", methods=["GET"])
+def get_task_id(id):
+    for task in tasks:
+        if task["id"] == id:
+            return jsonify(task)
+
+            
+
+if __name__ == "__main__":
+    app.run(debug=True)
