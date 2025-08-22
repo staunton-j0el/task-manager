e3133b7fa5cd86e589733b3578bde8df8f80a150
staunton-j0el <staunton.joel@gmail.com>
2025-08-22 21:23:12 +1000

    Adding PUT and DELETE functions

--- task_manager_day3/app.py
+++ task_manager_day3/app.py
@@ -18,6 +18,29 @@ def get_task_id(id):
             return jsonify(task)
     return jsonify({"error": "Task not found"}), 404
 
+@app.route("/tasks/<int:id>", methods=["PUT"])
+def update_task(id):
+    for task in tasks:
+        if task["id"] == id:
+            taskData = request.get_json()
+            if "title" in taskData:
+                task["title"] = taskData["title"]
+                return task
+            if "done" in taskData:
+                task["done"] = taskData["done"]
+                return task
+    else:
+        return jsonify({"error": "Task not avaialble"}), 404
+    
+@app.route("/tasks/<int:id>", methods=["DELETE"])
+def delete_task(id):
+    for task in tasks:
+        if task["id"] == id:
+            tasks.remove(task)
+            return jsonify({"success": "Task deleted"}), 200
+            
+    return jsonify({"error": "Task not avaialble"}), 404
+
 @app.route("/tasks", methods=["POST"])
 def create_task():
     data = request.get_json() # read json body
@@ -28,6 +51,8 @@ def create_task():
     }
     tasks.append(new_task)
     return jsonify(new_task), 201
+
+
 
 
 '''
